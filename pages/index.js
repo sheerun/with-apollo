59bdb6511ba1cfbdbc99d6097654a18040529180
Adam Stankiewicz <sheerun@sher.pl>
2018-11-26 21:35:25 +0100

    Verify google

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,13 @@ import App from '../components/App'
 import Header from '../components/Header'
 import Submit from '../components/Submit'
 import PostList from '../components/PostList'
+import Head from 'next/head'
 
 export default () => (
   <App>
+    <Head>
+      <meta name="google-site-verification" content="6Henc3W_kV_SFW-10sMb5wP9_28ftpKzIBGZHdhe1aA" />
+    </Head>
     <Header />
     <Submit />
     <PostList />
