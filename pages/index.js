d7c0363e9f6a35820cc566e46b603ecf58e07d5f
Adam Stankiewicz <sheerun@sher.pl>
2018-12-17 11:57:31 +0100

    Remove unnecessary files

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,7 @@
-import App from '../components/App'
-import Header from '../components/Header'
-import Submit from '../components/Submit'
-import PostList from '../components/PostList'
-import Head from 'next/head'
 import includes from 'awesome-includes'
 
 export default () => (
-  <App>
-    <Head>
-      <meta name="google-site-verification" content="6Henc3W_kV_SFW-10sMb5wP9_28ftpKzIBGZHdhe1aA" />
-    </Head>
+  <div>
     {includes([41,42]) ? 'awesome!' : 'not awesome...'}
-    <Header />
-    <Submit />
-    <PostList />
-  </App>
+  </div>
 )
